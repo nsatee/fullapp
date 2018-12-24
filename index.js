0a756741e890ebc7d2c441b7bb96923b674de636
Nattawod Satee <nattawodsatee@gmail.com>
2018-12-24 15:39:11 -0800

    complete auth

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 const keys = require('./config/keys');
+
 require('./modules/User');
 require('./services/passport');
 
@@ -8,6 +12,15 @@ require('./services/passport');
 mongoose.connect(keys.mogoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
