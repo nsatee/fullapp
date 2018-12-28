26a04e1acbdc16250bf2119a2f33ec1e4f5314c8
Nattawod Satee <nattawodsatee@gmail.com>
2018-12-28 15:27:56 -0800

    implement route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>
+const ServayNew = () => <h2>ServayNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
     return (
         <div>
-            Hi there!
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing}></Route>
+                    <Route exact path="/surveys" component={Dashboard}></Route>
+                    <Route path="/surveys/new" component={ServayNew}></Route>
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
