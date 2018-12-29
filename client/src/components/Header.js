3f14177827daf2644955a6960872e4749c21f4ab
Nattawod Satee <nattawodsatee@gmail.com>
2018-12-28 16:19:59 -0800

    implement full store with actions, reducers and middlewhere

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                <a className="left brand-logo">Emaily</a>
+                <ul id="nav-mobile" className="right">
+                    <li><a>Login with Google</a></li>
+                </ul>
+                </div>
+            </nav>
         );
     }
 }
